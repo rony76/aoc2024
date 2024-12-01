2431cdf587c051998e336379576c70e81e9184ef
Rony Cesana <rony.cesana@thoughtworks.com>
2024-12-01 22:43:36 +0100

    Day 1

---
+++ src/main/java/org/nalda/adventofcode2024/ResourceUtil.java
@@ -0,0 +1,41 @@
+package org.nalda.adventofcode2024Ø;
+
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ResourceUtil {
+    @SneakyThrows
+    private static Path getInputPath(String name) {
+        final URL resource = ResourceUtil.class.getClassLoader().getResource(name);
+        return new File(resource.toURI()).toPath();
+    }
+
+    @SneakyThrows
+    public static Stream<String> getLineStream(String name) {
+        return Files.lines(getInputPath(name));
+    }
+
+    @SneakyThrows
+    public static Supplier<Stream<String>> getLineStreamSupplier(String name) {
+        return () -> {
+            try {
+                return Files.lines(getInputPath(name));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    @SneakyThrows
+    public static List<String> getLineList(String name) {
+        return Files.readAllLines(getInputPath(name));
+    }
+}
