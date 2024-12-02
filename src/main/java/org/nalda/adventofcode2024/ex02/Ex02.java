1345b12a68ec73a1ad4efd066a0e23d1c7389c44
Rony Cesana <9611986+rony76@users.noreply.github.com>
2024-12-02 11:04:03 +0100

    Day 2, second star

--- src/main/java/org/nalda/adventofcode2024/ex02/Ex02.java
+++ src/main/java/org/nalda/adventofcode2024/ex02/Ex02.java
@@ -7,25 +7,37 @@ import static org.nalda.adventofcode2024Ø.ResourceUtil.*;
 public class Ex02 {
     public long countSafeReports(String inputName) {
         return getLineStream(inputName)
-                .map(Report::new)
+                .map(Report::createReport)
                 .filter(Report::isSafe)
+                .count();
+    }
+
+    public long countSafeReportsWithProblemDampener(String inputName) {
+        return getLineStream(inputName)
+                .map(Report::createReport)
+                .filter(Report::isSafeWithProblemDampener)
                 .count();
     }
 
     public static void main(String[] args) {
         final Ex02 ex02 = new Ex02();
         System.out.println(ex02.countSafeReports("ex02.input.txt"));
+        System.out.println(ex02.countSafeReportsWithProblemDampener("ex02.input.txt"));
     }
 
     static class Report {
-
         private final int[] levels;
 
-        public Report(String input) {
-            levels = Arrays.stream(input.split("\s+")).mapToInt(Integer::parseInt).toArray();
+        private Report(int[] levels) {
+            this.levels = levels;
+        }
+
+        public static Report createReport(String input) {
+            int[] levels = Arrays.stream(input.split("\s+")).mapToInt(Integer::parseInt).toArray();
             if (levels.length < 2) {
                 throw new IllegalArgumentException("Report must have at least 2 levels");
             }
+            return new Report(levels);
         }
 
         public boolean isSafe() {
@@ -41,7 +53,7 @@ public class Ex02 {
                     diff = -diff;
                 }
 
-                if (diff <= 0) {
+                if (diff < 1) {
                     return false;
                 }
 
@@ -51,6 +63,32 @@ public class Ex02 {
             }
 
             return true;
+        }
+
+        public boolean isSafeWithProblemDampener() {
+            if (isSafe()) {
+                return true;
+            }
+            for (int i = 0; i < levels.length; i++) {
+                if (subReportBut(i).isSafe()) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Report subReportBut(int index) {
+            int[] subLevels = new int[levels.length - 1];
+            for (int i = 0; i < levels.length - 1; i++) {
+                subLevels[i] = i < index ? levels[i] : levels[i + 1];
+            }
+
+            return new Report(subLevels);
+        }
+
+        @Override
+        public String toString() {
+            return "Report{" + Arrays.toString(levels) + '}';
         }
     }
 }
--- src/test/java/org/nalda/adventofcode2024/ex02/Ex02Test.java
+++ src/test/java/org/nalda/adventofcode2024/ex02/Ex02Test.java
@@ -14,13 +14,57 @@ public class Ex02Test {
 
     @Test
     void firstLineIsSafe() {
-        final Ex02.Report report = new Ex02.Report("7 6 4 2 1");
+        final Ex02.Report report = Ex02.Report.createReport("7 6 4 2 1");
         assertThat(report.isSafe()).isTrue();
     }
 
     @Test
     void secondLineIsNotSafe() {
-        final Ex02.Report report = new Ex02.Report("1 2 7 8 9");
+        final Ex02.Report report = Ex02.Report.createReport("1 2 7 8 9");
         assertThat(report.isSafe()).isFalse();
     }
+
+    @Test
+    void inputHas4SafeReportsWithProblemDampener() {
+        final Ex02 ex02 = new Ex02();
+
+        assertThat(ex02.countSafeReportsWithProblemDampener("ex02.input.txt")).isEqualTo(4);
+    }
+
+    @Test
+    void firstLineIsSafeWithProblemDampener() {
+        final Ex02.Report report = Ex02.Report.createReport("7 6 4 2 1");
+        assertThat(report.isSafeWithProblemDampener()).isTrue();
+    }
+
+    @Test
+    void secondLineIsNotSafeWithProblemDampener() {
+        final Ex02.Report report = Ex02.Report.createReport("1 2 7 8 9");
+        assertThat(report.isSafeWithProblemDampener()).isFalse();
+    }
+
+    @Test
+    void thirdLineIsNotSafeWithProblemDampener() {
+        final Ex02.Report report = Ex02.Report.createReport("9 7 6 2 1");
+        assertThat(report.isSafeWithProblemDampener()).isFalse();
+    }
+
+    @Test
+    void fourthLineIsSafeWithProblemDampener() {
+        final Ex02.Report report = Ex02.Report.createReport("1 3 2 4 5");
+        assertThat(report.isSafeWithProblemDampener()).isTrue();
+    }
+
+    @Test
+    void aLineSafeButLastIsSafe() {
+        final Ex02.Report report = Ex02.Report.createReport("1 2 4 6 5");
+        assertThat(report.isSafeWithProblemDampener()).isTrue();
+    }
+
+    @Test
+    void firstFailingFromRealInputShouldBeSafe() {
+        final Ex02.Report report = Ex02.Report.createReport("77 80 81 82 86");
+        assertThat(report.isSafeWithProblemDampener()).isTrue();
+    }
+
 }
