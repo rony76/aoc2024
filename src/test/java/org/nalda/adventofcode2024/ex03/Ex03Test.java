c476e2d20cac026c78705d19b8140d5690a1a8b0
Rony Cesana <rony.cesana@thoughtworks.com>
2024-12-03 07:07:20 +0100

    Day 3, first star

---
+++ src/test/java/org/nalda/adventofcode2024/ex03/Ex03Test.java
@@ -0,0 +1,18 @@
+package org.nalda.adventofcode2024.ex03;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Ex03Test {
+    @Test
+    void testSampleInput() {
+        final Ex03 ex03 = new Ex03();
+
+        final BigInteger actual = ex03.sumMultiplications("ex03.input.txt");
+
+        assertThat(actual).isEqualTo(BigInteger.valueOf(161));
+    }
+}
