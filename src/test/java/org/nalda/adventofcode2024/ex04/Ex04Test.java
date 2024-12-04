822b41177cf2aa9bf0df7f147c618218cc77f785
Rony Cesana <rony.cesana@thoughtworks.com>
2024-12-04 13:55:10 +0100

    Day 4, first star

---
+++ src/test/java/org/nalda/adventofcode2024/ex04/Ex04Test.java
@@ -0,0 +1,15 @@
+package org.nalda.adventofcode2024.ex04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Ex04Test {
+    @Test
+    void findXmasInInput() {
+        Ex04 ex04 = new Ex04("ex04.input.txt");
+        long xmases = ex04.countXmas();
+
+        assertThat(xmases).isEqualTo(18);
+    }
+}
